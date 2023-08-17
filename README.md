4fd19f3976c8cd83d5b7e68f1bc97d7a26b31275
cheangkokfook <141533007+cheangkokfook@users.noreply.github.com>
2023-08-17 13:23:07 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://cheangkokfook.github.io/l9110-motor/](https://cheangkokfook.github.io/l9110-motor/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/cheangkokfook/l9110-motor** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/cheangkokfook/l9110-motor** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,42 @@
+enum Motor {
+    M1_P8_P1,
+    M2_P9_P2
+}
+enum Motor_dir {
+    forward,
+    backward
+}
+namespace L9110_Car {
+
+    //% blockId="L9110M1" block="M1 (P8, P1)| dir %d"
+    export function set_dirM1(d: Motor_dir): void {
+        if (Motor_dir.forward) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+        }
+        if (Motor_dir.backward) {
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+        }
+    }
+
+    //% blockId="L9110M2" block="M2 (P9, P2)| dir %d"
+    export function set_dirM2(d: Motor_dir): void {
+        if (Motor_dir.forward) {
+            pins.digitalWritePin(DigitalPin.P9, 1)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+        }
+        if (Motor_dir.backward) {
+            pins.digitalWritePin(DigitalPin.P9, 0)
+            pins.digitalWritePin(DigitalPin.P2, 1)
+        }
+    }
+
+    //% blockId="L9110Stop" block="Motor Stop"
+    export function set_stop(): void {
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P9, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+}
